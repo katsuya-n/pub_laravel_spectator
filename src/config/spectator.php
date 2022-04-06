3d907e79bfc415a4f6bb5081386e375f49c2cbaa
katsuya <k_nakano@yumemi.co.jp>
2022-04-06 23:50:58 +0900

    hotmeteor/spectatorをインストール

---
+++ src/config/spectator.php
@@ -0,0 +1,69 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Spec Source
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default spec source that should be used
+    | by the framework.
+    |
+    */
+
+    'default' => env('SPEC_SOURCE', 'local'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sources
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure as many sources as you wish, and you
+    | may even configure multiple source of the same type. Defaulsourcets have
+    | been setup for each driver as an example of the required options.
+    |
+    */
+
+    'sources' => [
+        'local' => [
+            'source' => 'local',
+//            'base_path' => env('SPEC_PATH'),
+            'base_path' => 'ref',
+        ],
+
+        'remote' => [
+            'source' => 'remote',
+            'base_path' => env('SPEC_PATH'),
+            'params' => env('SPEC_URL_PARAMS', ''),
+        ],
+
+        'github' => [
+            'source' => 'github',
+            'base_path' => env('SPEC_GITHUB_PATH'),
+            'repo' => env('SPEC_GITHUB_REPO'),
+            'token' => env('SPEC_GITHUB_TOKEN'),
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Paths
+    |--------------------------------------------------------------------------
+    |
+    | Configure path defaults, like prefixes.
+    |
+    */
+
+    'path_prefix' => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Errors
+    |--------------------------------------------------------------------------
+    |
+    | Suppress errors in tests and only show messages.
+    |
+    */
+    'suppress_errors' => true,
+//    'suppress_errors' => false,
+];
